0d5ec8629f91f8f0a3d919931ed572c5667902ef
Ferit Gezgil <113307746+hcbilisim@users.noreply.github.com>
2025-01-21 13:43:17 +0300

    Update EasyPost.js

--- EasyPost.js
+++ EasyPost.js
@@ -5,11 +5,11 @@
     // Yardımcı Fonksiyonlar
     // ------------------------
 
-    function escapeHtml(text) {
-        if (!text) return "";
+   function escapeHtml(text) {
+    if (!text) return "";
         return text
             .replace(/&/g, "&amp;")
-            .replace(/<//g, "&lt;")
+            .replace(/</g, "&lt;")
             .replace(/>/g, "&gt;")
             .replace(/"/g, "&quot;")
             .replace(/'/g, "&#039;");
